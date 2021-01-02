67a00311e3ab9801532f024551b333b8a63b7a9e
Pavan Padmanabhi <pavan.padmanabhi@gmail.com>
2021-01-02 10:44:40 +0530

    SW-3|add transaction date and added new method to get account by name

--- src/main/java/com/pavan/app/repositories/AccountRepository.java
+++ src/main/java/com/pavan/app/repositories/AccountRepository.java
@@ -1,6 +1,7 @@
 package com.pavan.app.repositories;
 
 import com.pavan.app.entities.Account;
+import com.pavan.app.entities.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +9,6 @@ import java.util.UUID;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, UUID> {
+
+    Account findByAccountName(String accountName);
 }
