be3400e8c07e1a548ad98570c68a185e5e2e5458
Pavan Padmanabhi <11795349+pavanp73@users.noreply.github.com>
2021-01-03 15:00:47 +0530

    SW-8|fixed incorrect balance calculations after updating transaction (#9)

--- src/main/java/com/pavan/app/services/core/AccountService.java
+++ src/main/java/com/pavan/app/services/core/AccountService.java
@@ -1,18 +1,13 @@
 package com.pavan.app.services.core;
 
 import com.pavan.app.entities.Account;
-import com.pavan.app.entities.Transaction;
 import com.pavan.app.models.dto.AccountDto;
-import com.pavan.app.models.enums.OperationType;
-import com.pavan.app.models.enums.TransactionType;
 import com.pavan.app.repositories.AccountRepository;
 import com.pavan.app.services.mapper.AccountMapper;
 import com.pavan.app.services.mapper.base.AbstractMapper;
-import com.pavan.app.services.util.FinUtility;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -55,8 +50,6 @@ public class AccountService {
         accountToBeUpdated.setAccountName(accountDto.getAccountName());
         accountToBeUpdated.setAccountType(accountDto.getAccountType());
 
-        //For now only Account Name and Account Type can ne updated
-        //Todo - update initial amount
         return accountMapper.mapOneToDto(
                 accountRepository.save(accountToBeUpdated)
         );
@@ -69,47 +62,12 @@ public class AccountService {
         return "Account with " + accountId + " deleted successfully";
     }
 
-    public Account getByAccountName(String accountName){
+    Account getByAccountName(String accountName){
         return accountRepository.findByAccountName(accountName);
     }
 
-    void updateBalance(Transaction transaction, OperationType operationType){
-        TransactionType transactionType = TransactionType.of(transaction.getTransactionType());
-        switch (transactionType){
-            case EXPENSE -> {
-                Account sourceAccount = transaction.getAccount();
-                sourceAccount.setBalance(
-                        (OperationType.DELETE.equals(operationType)) ?
-                                FinUtility.credit(sourceAccount, transaction.getAmount()) :
-                                FinUtility.debit(sourceAccount, transaction.getAmount())
-                );
-                accountRepository.save(sourceAccount);
-            }
-            case INCOME -> {
-                Account sourceAccount = transaction.getAccount();
-                sourceAccount.setBalance(
-                        (OperationType.DELETE.equals(operationType)) ?
-                                FinUtility.debit(sourceAccount, transaction.getAmount()) :
-                                FinUtility.credit(sourceAccount, transaction.getAmount())
-                );
-                accountRepository.save(sourceAccount);
-            }
-            case TRANSFER -> {
-                Account sourceAccount = transaction.getAccount();
-                Account destinationAccount = transaction.getTransferToAccount();
-                if(OperationType.DELETE.equals(operationType)){
-                    sourceAccount.setBalance(FinUtility.credit(sourceAccount, transaction.getAmount()));
-                    destinationAccount.setBalance(FinUtility.debit(destinationAccount, transaction.getAmount()));
-                }
-                else{
-                    sourceAccount.setBalance(FinUtility.debit(sourceAccount, transaction.getAmount()));
-                    destinationAccount.setBalance(FinUtility.credit(destinationAccount, transaction.getAmount()));
-                }
-                List<Account> accounts = new ArrayList<>();
-                accounts.add(sourceAccount);
-                accounts.add(destinationAccount);
-                accountRepository.saveAll(accounts);
-            }
-        }
+    // Update account balance(s) when transaction is deleted
+    void updateAccounts(List<Account> accountList) {
+        accountRepository.saveAll(accountList);
     }
 }
